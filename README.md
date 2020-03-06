1f0f4ae283060509837a18826b3c74eaddb88a59
Will <doggy.huang@gmail.com>
2020-03-07 02:40:07 +0800

    加入 SB Admin 2 版型到 Angular 應用程式中 (僅首頁)
    https://startbootstrap.com/themes/sb-admin-2/

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Demo1
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.0.4.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.5.
 
 ## Development server
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,20 +16,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
   it(`should have as title 'demo1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('demo1');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('demo1 app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to demo1!');
   });
 });
