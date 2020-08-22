bac85cfbffbc6a427725e2b36e3b160840e5c212
Will <doggy.huang@gmail.com>
2020-08-22 09:51:18 +0800

    建立 page1 與 page2 頁面 (元件)

---
+++ src/app/page2/page2.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './page2.component.html',
+  styleUrls: ['./page2.component.css']
+})
+export class Page2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
