1c203da9c0ead33dc41a11fd11079062e349cbab
Will <doggy.huang@gmail.com>
2020-08-22 11:11:28 +0800

    移除 Home 選單連結

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
+// import { PathNotFoundComponent } from './path-not-found/path-not-found.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
