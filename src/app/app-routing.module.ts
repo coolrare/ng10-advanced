97937287b1ac654b58a7f73a795f890e81df0636
Will <doggy.huang@gmail.com>
2020-08-22 09:59:08 +0800

    完成 page1 與 page2 的路由定義

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'page1', component: Page1Component },
+  { path: 'page2', component: Page2Component },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
